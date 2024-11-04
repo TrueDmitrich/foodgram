e43bee34d549a21f3b36943527ab3797f470e2a7
Íèêèòà Õóäÿêîâ <hud.nik@yandex.ru>
2024-11-04 13:57:19 +0300

    v_0.91

--- backend/api/models.py
+++ backend/api/models.py
@@ -56,7 +56,7 @@ class Tag(models.Model):
 class Ingredient(models.Model):
     """Ингридиенты для рецептов."""
 
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=100)
     measurement_unit = models.CharField(max_length=50)
 
     class Meta:
