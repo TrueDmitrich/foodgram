fad2a2209959ec41be027cbc9e1423bf4abc2dbf
Íèêèòà Õóäÿêîâ <hud.nik@yandex.ru>
2024-11-08 19:37:54 +0300

    v_1.5

---
+++ backend/recipes/management/commands/fill_db.py
@@ -0,0 +1,21 @@
+# Заполнение базы
+from django.core.management import BaseCommand
+
+from recipes.models import Tag, Ingredient
+import csv
+
+
+tag_list = ['tag_1', 'tag_2', 'tag_3', 'tag_4', 'tag_5']
+tag_object_list = [Tag(name=tag, slug=tag) for tag in tag_list]
+
+
+class Command(BaseCommand):
+
+    def handle(self, *args, **options):
+        for tag in tag_object_list:
+            _, __ = Tag.objects.get_or_create(name=tag, slug=tag)
+        with open('ingredients.csv', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                _, __ = Ingredient.objects.get_or_create(
+                    name=row['name'], measurement_unit=row['unit'])
